450e18be4b37ca39796e2b02b9e9b7d60cf97c0b
jadhavjyotii <jadhavjr123@gmail.com>
2024-01-02 19:23:49 +0530

    sample program
    sample program

---
+++ helloWorld.js
@@ -0,0 +1,11 @@
+//console.log("hello world" + "Im here");
+//var declaration
+var num;
+console.log(num);
+var num="29";
+console.log(num);
+console.log("jyoti");
+var myName="jyoti"
+console.log(myName ,"jyoti");
+var num="null";
+console.log(num);
---
+++ logical_Operator.js
@@ -0,0 +1,5 @@
+var ssc = 65;
+var hsc = 70;
+var result =
+  ssc >= 50 && hsc >= 60 ? "allow for interview " : "dont allow for interview";
+console.log(`security guard is  saying   ${result}`);
